3e84e5ed3745dcc27c2fc5e12bd86871aa7725b5
Alexandre Courbot <gnurou@gmail.com>
2024-11-22 20:31:47 +0900

    strings: stop using unwrap and return errors

--- src/strings.rs
+++ src/strings.rs
@@ -4,8 +4,6 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::io::{self};
 
-use tracing::error;
-
 pub type GameStrings = BTreeMap<usize, String>;
 
 pub fn load_strings() -> io::Result<GameStrings> {
@@ -15,16 +13,10 @@ pub fn load_strings() -> io::Result<GameStrings> {
     let mut strings = GameStrings::new();
 
     for line in lines {
-        let line = match line {
-            Ok(line) => line,
-            Err(e) => {
-                error!("Error parsing string: {}", e);
-                continue;
-            }
-        };
+        let line = line?;
         let parts = line.split_at(5);
 
-        let index = usize::from_str_radix(&parts.0[2..], 16).unwrap();
+        let index = usize::from_str_radix(&parts.0[2..], 16).map_err(std::io::Error::other)?;
         let string = parts.1[2..].replace("\\n", "\n");
 
         strings.insert(index, string);
